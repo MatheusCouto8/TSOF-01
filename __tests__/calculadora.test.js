2e0684233d1fa18b8b3720a507ff3b2f43dfba77
Matheus Couto <158165513+MatheusCouto8@users.noreply.github.com>
2025-08-26 11:07:48 -0300

    feat: create multiplicar e dividir test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,5 +1,7 @@
 import { somar } from "../src/calculadora.js";
 import { subtrair } from "../src/calculadora.js";
+import { multiplicar } from "../src/calculadora.js";
+import { dividir } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("Deve somar dois numeros corretamente", () => {
@@ -27,7 +29,37 @@ describe("Calculadora", () => {
         // Assert (Verificar)
         expect(resultado).toBe(10);
     });
+
+
+    test("Deve multiplicar dois numeros corretamente", () => {
+        //AAA
+        //Arranjar (Preparar)
+        const a = 5;
+        const b = 3;
+
+        // Act (Executar)
+        const resultado = multiplicar(a, b);
+
+        // Assert (Verificar)
+        expect(resultado).toBe(15);
+    });
+
+    test("Deve dividir dois numeros corretamente", () => {
+        //AAA
+        //Arranjar (Preparar)
+        const a = 6;
+        const b = 3;
+
+        // Act (Executar)
+        const resultado = dividir(a, b);
+
+        // Assert (Verificar)
+        expect(resultado).toBe(2);
+    });
+
+    test("Não é possível dividir por zero", () => {
+        expect(() => {
+            dividir(5, 0);
+        }).toThrow("Não é possível dividir por zero");
+    });
 });
-
-
-    
